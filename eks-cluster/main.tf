5fb2849f3802ab7e9f8735911e43b17f86a78be3
ashrafgate <66882690+ashrafgate@users.noreply.github.com>
2025-01-08 17:50:20 +0530

    Update main.tf

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -66,9 +66,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     example = {
-      min_size     = 1
+      min_size     = 2
       max_size     = 10
-      desired_size = 1
+      desired_size = 2
 
       instance_types = ["t3.large"]
     }
